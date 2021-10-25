a0d016bc1f04a3dc4023d096c4cbf5c75547a6fa
Egor Ermilov <egorka33rus52@gmail.com>
2021-10-25 13:20:04 +0300

    created  validation and updated prettier and app.js

--- src/api.js
+++ src/api.js
@@ -6,53 +6,55 @@ const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_
 const AGGREGATE_INDEX = "5";
 
 socket.addEventListener("message", (e) => {
-  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
-  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
-    return;
-  }
+	const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
 
-  const handlers = tickersHandlers.get(currency) ?? [];
-  handlers.forEach((fn) => fn(newPrice));
+	if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+		return;
+	}
+	const handlers = tickersHandlers.get(currency) ?? [];
+	handlers.forEach((fn) => fn(newPrice));
 });
 
 function sendToWebSocket(message) {
-  const stringifiedMessage = JSON.stringify(message);
+	const stringifiedMessage = JSON.stringify(message);
 
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(stringifiedMessage);
-    return;
-  }
+	if (socket.readyState === WebSocket.OPEN) {
+		socket.send(stringifiedMessage);
+		return;
+	}
 
-  socket.addEventListener(
-    "open",
-    () => {
-      socket.send(stringifiedMessage);
-    },
-    { once: true }
-  );
+	socket.addEventListener(
+		"open",
+		(e) => {
+			const { TYPE: type } = JSON.parse(e.data);
+			console.log(type);
+			socket.send(stringifiedMessage);
+		},
+		{ once: true },
+	);
 }
 
 function subscribeToTickerOnWs(ticker) {
-  sendToWebSocket({
-    action: "SubAdd",
-    subs: [`5~CCCAGG~${ticker}~USD`],
-  });
+	sendToWebSocket({
+		action: "SubAdd",
+		subs: [`5~CCCAGG~${ticker}~USD`],
+	});
 }
 
 function unsubscribeFromTickerOnWs(ticker) {
-  sendToWebSocket({
-    action: "SubRemove",
-    subs: [`5~CCCAGG~${ticker}~USD`],
-  });
+	sendToWebSocket({
+		action: "SubRemove",
+		subs: [`5~CCCAGG~${ticker}~USD`],
+	});
 }
 
 export const subscribeToTicker = (ticker, cb) => {
-  const subscribers = tickersHandlers.get(ticker) || [];
-  tickersHandlers.set(ticker, [...subscribers, cb]);
-  subscribeToTickerOnWs(ticker);
+	const subscribers = tickersHandlers.get(ticker) || [];
+	tickersHandlers.set(ticker, [...subscribers, cb]);
+	subscribeToTickerOnWs(ticker);
 };
 
 export const unsubscribeFromTicker = (ticker) => {
-  tickersHandlers.delete(ticker);
-  unsubscribeFromTickerOnWs(ticker);
+	tickersHandlers.delete(ticker);
+	unsubscribeFromTickerOnWs(ticker);
 };
